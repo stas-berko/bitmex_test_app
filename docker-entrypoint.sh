83e94b7d57acbc25b646db90889c43d1b9f269e4
Stanislav Berko <uselesstas@gmail.com>
2020-06-09 22:25:30 +0300

    [ci] prepare docker environment

---
+++ docker-entrypoint.sh
@@ -0,0 +1,8 @@
+echo "Apply database migrations"
+python manage.py makemigrations
+
+echo "Apply database migrations"
+python manage.py migrate
+
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
